973b92ae190559f5671a39a2189131ecbf1cd051
gagenikolov33 <gagenikolov33@gmail.com>
2024-11-10 17:31:01 +0100

    -tradingjsx should be stuctured and devided into components

--- frontend/src/Trading.jsx
+++ frontend/src/Trading.jsx
@@ -738,43 +738,6 @@ const TradingApp = () => {
         // setSpotPositionsCount(count)
       }
 
-      // if (data.closedSpotPositions) {
-      //   count = data.closedSpotPositions.length;
-      //   if (count !== spotClosedPositionsCount)
-      //     spotClosedPositionsDiv.innerHTML = ""; // Clear previous closed positions
-
-      //   data.closedSpotPositions.forEach((position) => {
-      //     if (position.positionType == "sell" && position.orderLimit == 0) {
-      //       spotBalances[assetTypes.indexOf(position.assetType) + 1] -=
-      //         position.amount;
-      //     }
-      //     let positionDiv = document.createElement("div");
-
-      //     if (count !== spotClosedPositionsCount) {
-      //       positionDiv.textContent = `
-      //                               ${1 - position.orderLimit}-${position.orderType
-      //         }-${position.positionType}-
-      //                               Amount: ${position.amount}(${position.assetType
-      //         }),
-      //                               Entry Price: ${position.entryPrice}(USDT),
-      //                           `;
-      //       if (position.orderType == "limit")
-      //         positionDiv.textContent += ` Limit Price: $${position.limitPrice},`;
-      //       if (position.orderType == "limit") {
-      //         if (position.orderLimit == 1) {
-      //           if (position.positionType == "sell")
-      //             positionDiv.style.backgroundColor = "#295843";
-      //           if (position.positionType == "buy")
-      //             positionDiv.style.backgroundColor = "#694456";
-      //         }
-      //       }
-
-      //       spotClosedPositionsDiv.appendChild(positionDiv);
-      //     }
-      //   });
-      //   spotClosedPositionsCount = count;
-      //   // setSpotClosedPositionsCount(count)
-      // }
 
       if (spotCurrentPrices.length) {
         spotBalancesUpdate();
